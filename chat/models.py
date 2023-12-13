476d47983e658d31119f095b24ef22e6dd9c77f9
HansStieber <hans.stieber@posteo.de>
2023-12-13 16:18:54 +0100

    added chat to Message model

--- chat/models.py
+++ chat/models.py
@@ -3,6 +3,9 @@ from datetime import date
 from django.conf import settings
 
 # Create your models here.
+class Chat(models.Model):
+  created_at = models.DateField(default=date.today)
+  
 class Message(models.Model):
   text = models.CharField(max_length=500)
   created_at =models.DateField(default=date.today)
@@ -14,6 +17,11 @@ class Message(models.Model):
   receiver = models.ForeignKey(
     settings.AUTH_USER_MODEL,
     on_delete=models.CASCADE, related_name='receiver_message_set')
-  
-class Chat(models.Model):
-  created_at = models.DateField(default=date.today)
+  chat = models.ForeignKey(
+    Chat,
+    on_delete=models.CASCADE,
+    related_name='chat_message_set',
+    default=None,
+    blank=True,
+    null=True
+  )
