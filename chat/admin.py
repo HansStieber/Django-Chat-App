2e0bc6c7007d196c460695be366216f850cdf792
HansStieber <hans.stieber@posteo.de>
2023-12-13 15:46:36 +0100

    changed display on message overview at admin interface

--- chat/admin.py
+++ chat/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Message
 
 class MessageAdmin(admin.ModelAdmin):
-    field=('text', 'created_at', 'author', 'receiver')
+    field=('text', 'created_at', 'author', 'receiver',)
+    list_display=('created_at', 'author', 'receiver',)
+    search_fields=('text',)
 
 
 # Register your models here.
