41e078e685bd61db01ab4536c919711ffa787ae9
HansStieber <hans.stieber@posteo.de>
2023-12-13 15:07:07 +0100

    added input and button to index.html

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from chat.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('chat/', index),
+  path('admin/', admin.site.urls),
+  path('chat/', index),
 ]
