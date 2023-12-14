4b64940db4757aebf19a227f2eeb7ebb7601fab9
HansStieber <hans.stieber@posteo.de>
2023-12-14 13:32:46 +0100

    created login.html; added path at urls.py; added login_view at views.py

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from .models import Chat, Message
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def index(request):
@@ -14,3 +16,17 @@ def index(request):
       )
   chatMessages = Message.objects.filter(chat__id=chatId)
   return render(request, 'chat/index.html', {'messages': chatMessages})
+
+
+def login_view(request):
+  if request.method == 'POST':
+    user = authenticate(
+      username=request.POST.get('username'),
+      password=request.POST.get('password')
+      )
+    if user:
+      login(request, user)
+      return HttpResponseRedirect('/chat/')
+    else:
+      return render(request, 'auth/login.html', {'wrongPassword': True})
+  return render(request, 'auth/login.html')
