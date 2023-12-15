d1e4b7a87822942809f4d29cd5f8f0d12a584233
HansStieber <hans.stieber@posteo.de>
2023-12-15 16:56:44 +0100

    sending message is now handled by javascript

--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,8 @@ from .models import Chat, Message
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.core import serializers
 
 # Create your views here.
 @login_required(login_url='/login/')
@@ -10,18 +12,23 @@ def index(request):
   chatId = 4
   myChat = get_object_or_404(Chat, id=chatId)
   if request.method == 'POST':
-    Message.objects.create(
+    new_message = Message.objects.create(
       text=request.POST['textmessage'],
       chat=myChat,
       author=request.user,
       receiver=request.user, 
       )
-    return redirect('/chat/')
+    serialized_object = serializers.serialize('json', [ new_message, ])
+    return JsonResponse(serialized_object[1:-1], safe=False)
   chatMessages = Message.objects.filter(chat__id=chatId)
   return render(request, 'chat/index.html', {'messages': chatMessages})
 
 
 def login_view(request):
+  if request.GET.get('next') != None:
+    next = request.GET.get('next')
+  else:
+    next = '/chat/'
   if request.method == 'POST':
     user = authenticate(
       username=request.POST.get('username'),
@@ -29,10 +36,10 @@ def login_view(request):
       )
     if user:
       login(request, user)
-      return redirect('/chat/')
+      return redirect(request.POST.get('next'))
     else:
       return render(request, 'auth/login.html', {'wrongPassword': True})
-  return render(request, 'auth/login.html')
+  return render(request, 'auth/login.html', {'next': next})
 
 
 def register_view(request):
